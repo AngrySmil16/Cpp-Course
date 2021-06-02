af002bde3221075c1bd7ba95a297122ae37e6cac
CreperFulmen16 <67227839+AngrySmil16@users.noreply.github.com>
2021-06-02 17:25:26 +0200

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "There once was a man named George" << endl;
+    cout << "He was 2 years old" << endl;
+    cout << "He liked the name George" << endl;
+    cout << "But did not like being 2" << endl;
+    return 0;
+}
